bc87e88c344e5c25af917e7736515225103ddb25
mark-feher <maark.feheer@gmail.com>
2021-07-15 13:01:58 +0200

    First 2 exercises in Part 10.1 "Handling collections as streams"
    Finished 2 exercises in Part 10.1

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,12 +1,32 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
         // Write your program here
+        System.out.println("Input numbers, type \"end\" to stop.");
 
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("end")) {
+                break;
+            }
+
+            inputs.add(userInput);
+        }
+
+        double average = inputs.stream()
+                .mapToInt(string -> Integer.valueOf(string)) //  lambda can be replaced by Integer::valueOf
+                .average()
+                .getAsDouble();
+
+        System.out.println("average of the numbers: " + average);
     }
 }
--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,49 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        ArrayList<String> inputs = new ArrayList<>();
 
+        System.out.println("Input numbers, type \"end\" to stop.");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("end")) {
+                break;
+            }
+            if (userInput.isEmpty()) {
+                continue;
+            }
+            inputs.add(userInput);
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("p")) {
+            double average = inputs.stream()
+                    .mapToInt(string -> Integer.valueOf(string))    // map the stream of strings into integers
+                    .filter(integer -> integer > 0) // Filter out the positive numbers
+                    .average()
+                    .getAsDouble();
+            System.out.println("Average of the positive numbers: " + average);
+        } else if (userInput.equals("n")) {
+            double average = inputs.stream()
+                    .mapToInt(string -> Integer.valueOf(string))    // map the stream of strings into integers
+                    .filter(integer -> integer < 0) // Filter out the negative numbers
+                    .average()
+                    .getAsDouble();
+            System.out.println("Average of the negative numbers: " + average);
+        }
+
+        // note, if I choose e.g. to calculate negative avg when inputs is empty,
+        // I get an exception "No value present at.."
+        // "else if" seems to fix it
     }
 }
